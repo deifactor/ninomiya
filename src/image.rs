8258e37f575d2cc9b24f8da86eef82a60536e45d
Ash <relativistic.policeman@gmail.com>
2020-05-15 00:05:07 -0700

    Add icon loader code for just not using a GTK icon theme.
    This also means we can avoid doing the test runner thread thing for now,
    since if we don't use an icon loader we don't need to init GTK.

--- src/image.rs
+++ src/image.rs
@@ -12,20 +12,18 @@ pub struct Loader {
 }
 
 impl Loader {
+    /// Constructs a loader that will use the default GTK icon theme.
     pub fn new() -> Self {
-        Loader::new_with_icon_theme(None)
+        let theme = IconTheme::get_default();
+        if theme.is_none() {
+            warn!("Failed to get GTK icon theme");
+        }
+        Loader::new_with_icon_theme(theme)
     }
 
     /// Constructs an image loader that will use the given icon theme. Passing `None` will result
-    /// in using the default icon theme; if this can't be loaded, we will emit a warning.
+    /// in using no icon theme.
     pub fn new_with_icon_theme(icon_theme: Option<IconTheme>) -> Self {
-        let icon_theme = icon_theme.or_else(|| {
-            let theme = IconTheme::get_default();
-            if theme.is_none() {
-                warn!("Failed to get GTK icon theme");
-            }
-            theme
-        });
         Loader { icon_theme }
     }
 
@@ -70,57 +68,47 @@ impl Loader {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::gtk_test_runner::run_test;
     use std::path::PathBuf;
 
     #[test]
     pub fn load_builtins() -> Result<()> {
-        run_test(|| -> Result<()> {
-            let loader = Loader::new();
-            let demo_icon = loader
-                .load_from_path("ninomiya:///demo-icon.png")
-                .context("failed to load demo icon")?;
-            assert_eq!(demo_icon.get_width(), 200);
-            assert_eq!(demo_icon.get_height(), 200);
+        let loader = Loader::new_with_icon_theme(None);
+        let demo_icon = loader
+            .load_from_path("ninomiya:///demo-icon.png")
+            .context("failed to load demo icon")?;
+        assert_eq!(demo_icon.get_width(), 200);
+        assert_eq!(demo_icon.get_height(), 200);
 
-            let demo_image = loader
-                .load_from_path("ninomiya:///demo-image.png")
-                .context("failed to load demo image")?;
-            assert_eq!(demo_image.get_width(), 133);
-            assert_eq!(demo_image.get_height(), 190);
-            Ok(())
-        })
+        let demo_image = loader
+            .load_from_path("ninomiya:///demo-image.png")
+            .context("failed to load demo image")?;
+        assert_eq!(demo_image.get_width(), 133);
+        assert_eq!(demo_image.get_height(), 190);
+        Ok(())
     }
 
     #[test]
     pub fn load_nonexistent_from_disk() {
-        run_test(|| {
-            assert!(Loader::new()
-                .load_from_path("file:///404/not/found")
-                .is_err())
-        })
+        assert!(Loader::new_with_icon_theme(None)
+            .load_from_path("file:///404/not/found")
+            .is_err())
     }
 
     #[test]
     pub fn load_from_disk() -> Result<()> {
-        run_test(|| -> Result<()> {
-            let path = PathBuf::from("data/demo-image.png").canonicalize()?;
-            let url =
-                url::Url::from_file_path(path).map_err(|_| anyhow!("failed to convert url"))?;
-            let image = Loader::new().load_from_path(url.as_str())?;
-            assert_eq!(image.get_width(), 133);
-            assert_eq!(image.get_height(), 190);
-            Ok(())
-        })
+        let path = PathBuf::from("data/demo-image.png").canonicalize()?;
+        let url = url::Url::from_file_path(path).map_err(|_| anyhow!("failed to convert url"))?;
+        let image = Loader::new_with_icon_theme(None).load_from_path(url.as_str())?;
+        assert_eq!(image.get_width(), 133);
+        assert_eq!(image.get_height(), 190);
+        Ok(())
     }
 
     #[test]
     pub fn load_nonexistent_builtin() {
-        run_test(|| {
-            let loader = Loader::new();
-            assert!(loader
-                .load_from_path("ninomiya:///i-do-not-exist.png")
-                .is_err())
-        });
+        let loader = Loader::new_with_icon_theme(None);
+        assert!(loader
+            .load_from_path("ninomiya:///i-do-not-exist.png")
+            .is_err())
     }
 }
