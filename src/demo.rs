8983ee5b146db4bcff4e339017e13a61651d00e0
Ash <relativistic.policeman@gmail.com>
2020-05-16 20:00:59 -0700

    Remove some duplication in specifying demo notifications.

--- src/demo.rs
+++ src/demo.rs
@@ -18,52 +18,53 @@ pub fn send_notifications(tx: glib::Sender<NinomiyaEvent>) -> Result<()> {
 
 /// The list of notifications to send for demo purposes.
 fn demo_notifications() -> Vec<Notification> {
+    let base = || Notification {
+        id: 0,
+        icon: None,
+        actions: vec![],
+        application_name: Some("galax".into()),
+        summary: "placeholder".into(),
+        body: None,
+        hints: Hints::new(),
+    };
+
     let demo_icon = ImageRef::Url(demo_icon_url());
     let demo_image = ImageRef::Url(demo_image_url());
     let no_icon_no_image = Notification {
         id: 1,
-        icon: None,
-        actions: vec![],
-        application_name: Some("galax".into()),
         summary: "no image or icon".into(),
         body: Some("we are not alone / yowaku te tsuyoi hitori hitori da".into()),
-        hints: Hints::new(),
+        ..base()
     };
     let icon_no_image = Notification {
         id: 2,
         icon: Some(demo_icon.clone()),
-        actions: vec![],
-        application_name: Some("galax".into()),
         summary: "icon, no image".into(),
         body: Some("<load_galax> let's upgrade the world!".into()),
-        hints: Hints::new(),
+        ..base()
     };
     let no_icon_image = Notification {
         id: 3,
-        icon: None,
-        application_name: Some("galax".into()),
-        actions: vec![],
         summary: "image, no icon".into(),
         body: Some("<load_galax> gatchaman crowds is a good anime".into()),
         hints: Hints {
             image: Some(demo_image.clone()),
         },
+        ..base()
     };
     let image_icon = Notification {
         id: 4,
         icon: Some(demo_icon.clone()),
-        application_name: Some("galax".into()),
-        actions: vec![],
         summary: "image and icon".into(),
         body: Some("<load_galax> some weird alien gave me this book".into()),
         hints: Hints {
             image: Some(demo_image.clone()),
         },
+        ..base()
     };
     let image_and_actions = Notification {
         id: 5,
         icon: Some(demo_icon.clone()),
-        application_name: Some("galax".into()),
         summary: "image and actions".into(),
         body: Some("<load_galax> what will you do?".into()),
         hints: Hints {
@@ -79,6 +80,7 @@ fn demo_notifications() -> Vec<Notification> {
                 label: "perish like a MESS".into(),
             },
         ],
+        ..base()
     };
     vec![
         no_icon_no_image,
